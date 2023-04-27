38c9904cd1dabf28d9a1694836b565f26c5d757b
Tasnim Ahmed <tasnim@din.global>
2023-04-27 12:35:38 +0500

    do not compile if binary exists

--- unpub/entrypoint.sh
+++ unpub/entrypoint.sh
@@ -1,5 +1,10 @@
-#!/bin/sh
-cd /app
-dart pub get
-dart compile exe bin/unpub.dart -o bin/unpub
+#!/bin/bash
+
+FILE=/app/bin/unpub
+if [ ! -f "$FILE" ]; then
+    cd /app
+    dart pub get
+    dart compile exe bin/unpub.dart -o bin/unpub
+fi
+
 /app/bin/unpub --host $HOST --port $PORT --database $DATABASE
